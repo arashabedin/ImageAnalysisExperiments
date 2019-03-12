f67d4748250450e03696f89b047ba3d7e47018c4
arashabedin <arash.abedin@gmail.com>
2019-03-12 12:00:02 +0100

    Add image alignment using Sift Operator

--- Canny-Edge-Detection.py
+++ Canny-Edge-Detection.py
@@ -3,14 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
 
-img = cv2.imread('input_photos/CroquisModel.png',0)
-edges = cv2.Canny(img,20,120)
+img = cv2.imread('input_images/box/model_edited.jpg',0)
+
+#enhancing the contrast
+def contrastEnhancer(img): 
+    hist,bins = np.histogram(img.flatten(),256,[0,80])
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * hist.max()/ cdf.max()
+    cdf_m = np.ma.masked_equal(cdf,0)
+    cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+    cdf = np.ma.filled(cdf_m,0).astype('uint8')
+    return cdf[img]
+
+
+edges = cv2.Canny(contrastEnhancer(img) ,60,120)
 edges = cv2.bitwise_not(edges)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.imshow(edges,cmap = 'gray')
 
 plt.show()
 
--- Shi-Tomasi-Corner_Detection.py
+++ Shi-Tomasi-Corner_Detection.py
@@ -2,13 +2,86 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
+from numpy.core.umath_tests import inner1d
+
+
+def HausdorffDist(A,B):
+    # Hausdorf Distance: Compute the Hausdorff distance between two point
+    # clouds.
+    # Let A and B be subsets of metric space (Z,dZ),
+    # The Hausdorff distance between A and B, denoted by dH(A,B),
+    # is defined by:
+    # dH(A,B) = max(h(A,B),h(B,A)),
+    # where h(A,B) = max(min(d(a,b))
+    # and d(a,b) is a L2 norm
+    # dist_H = hausdorff(A,B)
+    # A: First point sets (MxN, with M observations in N dimension)
+    # B: Second point sets (MxN, with M observations in N dimension)
+    # ** A and B may have different number of rows, but must have the same
+    # number of columns.
+    #
+    # Edward DongBo Cui; Stanford University; 06/17/2014
+
+    # Find pairwise distance
+    D_mat = np.sqrt(inner1d(A,A)[np.newaxis].T + inner1d(B,B)-2*(np.dot(A,B.T)))
+    # Find DH
+    dH = np.max(np.array([np.max(np.min(D_mat,axis=0)),np.max(np.min(D_mat,axis=1))]))
+    return(dH)
+
+def ModHausdorffDist(A,B):
+    #This function computes the Modified Hausdorff Distance (MHD) which is
+    #proven to function better than the directed HD as per Dubuisson et al.
+    #in the following work:
+    #
+    #M. P. Dubuisson and A. K. Jain. A Modified Hausdorff distance for object
+    #matching. In ICPR94, pages A:566-568, Jerusalem, Israel, 1994.
+    #http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=576361
+    #
+    #The function computed the forward and reverse distances and outputs the
+    #maximum/minimum of both.
+    #Optionally, the function can return forward and reverse distance.
+    #
+    #Format for calling function:
+    #
+    #[MHD,FHD,RHD] = ModHausdorffDist(A,B);
+    #
+    #where
+    #MHD = Modified Hausdorff Distance.
+    #FHD = Forward Hausdorff Distance: minimum distance from all points of B
+    #      to a point in A, averaged for all A
+    #RHD = Reverse Hausdorff Distance: minimum distance from all points of A
+    #      to a point in B, averaged for all B
+    #A -> Point set 1, [row as observations, and col as dimensions]
+    #B -> Point set 2, [row as observations, and col as dimensions]
+    #
+    #No. of samples of each point set may be different but the dimension of
+    #the points must be the same.
+    #
+    #Edward DongBo Cui Stanford University; 06/17/2014
+
+    # Find pairwise distance
+    D_mat = np.sqrt(inner1d(A,A)[np.newaxis].T + inner1d(B,B)-2*(np.dot(A,B.T)))
+    # Calculating the forward HD: mean(min(each col))
+    FHD = np.mean(np.min(D_mat,axis=1))
+    # Calculating the reverse HD: mean(min(each row))
+    RHD = np.mean(np.min(D_mat,axis=0))
+    # Calculating mhd
+    MHD = np.max(np.array([FHD, RHD]))
+    return(MHD, FHD, RHD)
+
+
 
 #######################################
 ##### Shi Tomasi Corner Detection #####
 #######################################
 
-img = cv2.imread('results/Canny-Edge-Detected.png')
+img = cv2.imread('results/Edge_Detected_Box.png')
+img = cv2.resize(img,(800,600))
+
 height, width, channels = img.shape
+print(height)
+print(width)
+
 #This time we would add the corners to a white blank image
 blank = np.zeros([height,width,3],dtype=np.uint8)
 blank.fill(255)
@@ -17,9 +90,28 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(gray,225,0.01,10)
 corners = np.int0(corners)
 
+
+img2 = cv2.imread('results/Canny-Edge-Detected(rotated).png')
+gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+corners2 = cv2.goodFeaturesToTrack(gray2,225,0.01,10)
+corners2 = np.int0(corners2)
+
+
+n1 = np.squeeze(np.asarray(corners))
+n2 = np.squeeze(np.asarray(corners2))
+
+
+
+print(HausdorffDist(n1,n2))
+
+
+
+
 for i in corners:
     x,y = i.ravel()
     cv2.circle(blank,(x,y),3,255,-1)
+
 
 
 ########################################
